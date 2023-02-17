23d94245ac57ab0b481c9368a9edde3e894c7aeb
TheGhostOfInky <theghostofinky@gmail.com>
2023-02-17 14:42:00 +0000

    Added safeguards and made code more readable

--- passgen/passgen.c
+++ passgen/passgen.c
@@ -1,27 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "sodium.h"
+#include <sodium.h>
 
 char gen() {
-    char i = randombytes_uniform(62) + 0x30;
-    if (i > 0x39) {
-        i += 7;
-    }
-    if (i > 0x5A) {
-        i += 6;
-    }
-    return i;
+	char i = randombytes_uniform(62) + '0';
+	if (i > '9') {
+		i += 7;
+	}
+	if (i > 'Z') {
+		i += 6;
+	}
+	return i;
 }
 
-int main(int argc, char* argv[]){
-    int len = 16;
-    if (argv[1]) {
-        len = atoi(argv[1]);
-    }
-    char* arr;
-    arr = calloc(1,len+1);
-    for (int i = 0; i < len; i++) {
-        arr[i] = gen();
-    }
-    printf("%s", arr);
+int main(int argc, char* argv[]) {
+	int len = 16;
+	if (argv[1]) {
+		len = atoi(argv[1]);
+	}
+	if (len > (INT_MAX - 1) || len < 1) {
+		exit(7);
+	}
+	char* arr;
+	arr = malloc(len + 1);
+	if (arr == NULL) {
+		exit(22);
+	}
+	for (int i = 0; i < len; i++) {
+		arr[i] = gen();
+	}
+	arr[len] = 0;
+	puts(arr);
+	exit(0);
 }
